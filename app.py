c2b404d314b2227df0a5fbb4202a2985bc188cf9
RestGenius <restgenius.ai@gmail.com>
2025-08-05 02:04:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,20 @@ import io
 import json
 from datetime import datetime, timedelta
 from models import db, User
+from itsdangerous import URLSafeTimedSerializer
+from flask_mail import Mail, Message
 
 # Ініціалізація Flask-додатку
 app = Flask(__name__)
+# Налаштування пошти
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = os.getenv("EMAIL_USER")
+app.config["MAIL_PASSWORD"] = os.getenv("EMAIL_PASS")
+
+mail = Mail(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY", "mysecret")
 
