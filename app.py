38f8b207370d7769e009bd7051491043585edf62
RestGenius <restgenius.ai@gmail.com>
2025-08-05 07:50:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USERNAME"] = os.getenv("EMAIL_USER")
 app.config["MAIL_PASSWORD"] = os.getenv("EMAIL_PASS")
+app.config["MAIL_DEFAULT_SENDER"] = os.getenv("EMAIL_USER")  # 👈 Додай цей рядок
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "mysecret")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
 
