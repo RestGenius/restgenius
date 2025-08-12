9127bb054f2ed12c2b4e8b38461af9692f5bdcad
RestGenius <restgenius.ai@gmail.com>
2025-08-12 10:39:11 +0300

    Update models.py

--- models.py
+++ models.py
@@ -10,12 +10,16 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(200), nullable=False)
     is_verified = db.Column(db.Boolean, default=False)
 
+    # Статус підписки
+    is_pro = db.Column(db.Boolean, default=False)  # PRO користувач чи ні
+
     # Ліміти для Free-версії
     free_reports_used = db.Column(db.Integer, default=0)  # Кількість використаних звітів
     free_reports_reset = db.Column(db.DateTime, default=datetime.utcnow)  # Дата останнього скидання
 
     # 🔗 Зв'язок із Report
     reports = db.relationship('Report', backref='user', lazy=True)
+
 
 class Report(db.Model):
     id = db.Column(db.Integer, primary_key=True)
